05261d1b2d11b2ae70bcb129069f1c4cfd4d5c35
Amy Burnett <16aburnett@gmail.com>
2025-07-19 17:44:40 -0400

    CeruleanLD: Implemented handling symbols across multiple files with a working example

--- ceruleanld/linker.py
+++ ceruleanld/linker.py
@@ -1,3 +1,7 @@
+# CeruleanLD Linker: Processes and combines 1 or more CeruleanObj object code files into a single
+# CeruleanBC bytecode file.
+# =================================================================================================
+
 import os
 import sys
 import argparse
@@ -8,23 +12,54 @@ import json
 class Linker:
     def __init__(self, debug=False):
         self.shouldPrintDebug = debug
-        pass
 
     def printDebug (self, *args, **kwargs):
         """Custom debug print function."""
         if (self.shouldPrintDebug):
-            print(*args, **kwargs)
+            print (*args, **kwargs)
+
+    # ---------------------------------------------------------------------------------------------
 
     def link (self, objectCodes:list):
+        globalSymbolTable = self.buildGlobalSymbolTable (objectCodes)
         # Assign base addresses to each section
         self.assignSectionBaseAddresses (objectCodes)
         # Update the addresses of the symbol definitions
         self.resolveSymbolAddresses (objectCodes)
-        # Fill in the final address for the symbol references
+        # Update extern symbols with the correct addresses
+        # Note this needs to be a separate step as we need all the global symbols to be resolved
+        # first before we can fill in the extern references
+        self.resolveExternSymbolAddresses (objectCodes, globalSymbolTable)
+        # Fill in the final addresses for the symbol references in the code
         self.applyRelocations (objectCodes)
         # Put all the code together
         finalBytecode = self.flattenCodeSections (objectCodes)
         return finalBytecode
+
+    # ---------------------------------------------------------------------------------------------
+
+    def buildGlobalSymbolTable (self, objectCodes):
+        globalSymbolTable = {}
+        for objectCode in objectCodes:
+            for symbol in objectCode["symbols"]:
+                # Ensure it is a global symbol
+                symbolData = objectCode["symbols"][symbol]
+                if symbolData["visibility"] != "global":
+                    continue
+                # add filename for debug
+                symbolData["originalFilename"] = objectCode["filename"]
+                # Ensure symbol does not already exist
+                if symbol in globalSymbolTable:
+                    originalSymbolDecl = globalSymbolTable[symbol]
+                    print (f"ERROR: Duplicate global symbol '{symbol}'")
+                    print (f"   Found in: '{originalSymbolDecl["originalFilename"]}'")
+                    print (f"   Also defined in: '{symbolData["originalFilename"]}'")
+                    print (f"   Hint: Global symbols must be uniquely defined across object files. Consider renaming or adjusting visibility.")
+                    exit (1)
+                globalSymbolTable[symbol] = symbolData
+        return globalSymbolTable
+
+    # ---------------------------------------------------------------------------------------------
 
     def assignSectionBaseAddresses (self, objectCodes, baseStart=0x0000):
         self.printDebug ("Assigning section base addresses...")
@@ -39,12 +74,42 @@ class Linker:
             alignmentBytes = 8
             while currentBaseAddress % alignmentBytes != 0:
                 currentBaseAddress += 1
-    
+
+    # ---------------------------------------------------------------------------------------------
+
+    # This determines the absolute addresses for each symbol
+    # absAddress = sectionBaseAddress + relAddress
     def resolveSymbolAddresses (self, objectCodes):
         self.printDebug ("Resolving symbol addresses...")
         for objectCode in objectCodes:
+            symbolTable = objectCode["symbols"]
             for symbol in objectCode["symbols"]:
-                objectCode["symbols"][symbol]["relAddress"] += objectCode["baseAddress"]
+                symbolTable[symbol]["absAddress"] = objectCode["baseAddress"] + symbolTable[symbol]["relAddress"]
+
+    # ---------------------------------------------------------------------------------------------
+
+    # Files that use global symbols have extern symbol table entries
+    # This ensures that extern symbol table entries use the real address for the global symbols
+    def resolveExternSymbolAddresses (self, objectCodes, globalSymbolTable):
+        self.printDebug ("Resolving extern symbol addresses...")
+        for objectCode in objectCodes:
+            symbolTable = objectCode["symbols"]
+            for symbol in symbolTable:
+                # ensure it is an extern symbol
+                if symbolTable[symbol]["visibility"] != "extern":
+                    continue
+                # Ensure extern symbol exists in another file
+                if symbol not in globalSymbolTable:
+                    print (f"ERROR: Undefined extern symbol '{symbol}'")
+                    print (f"   Referenced in object file: '{objectCode['filename']}'")
+                    print (f"   No global definition found in linker inputs")
+                    exit (1)
+                # Use the actual address for the extern symbol
+                symbolTable[symbol]["absAddress"] = globalSymbolTable[symbol]["absAddress"]
+                self.printDebug (f"Resolving extern symbol '{symbol}' from '{objectCode['filename']}'")
+                self.printDebug (f"     with global symbol '{symbol}' from '{globalSymbolTable[symbol]['originalFilename']}' at address {globalSymbolTable[symbol]["absAddress"]:x}")
+
+    # ---------------------------------------------------------------------------------------------
 
     # Processes the address based on relocType and returns the (little endian) bytes to patch
     def convertAddressToBytes (self, address, relocType):
@@ -63,6 +128,9 @@ class Linker:
         print (f"ERROR: Unknown relocation type '{relocType}'")
         exit (1)
 
+    # ---------------------------------------------------------------------------------------------
+
+    # Fixes any places in the code where the absolute address of a symbol is needed
     def applyRelocations (self, objectCodes):
         self.printDebug ("Applying relocations...")
         for objectCode in objectCodes:
@@ -71,7 +139,7 @@ class Linker:
                 if relocation["symbol"] not in objectCode["symbols"]:
                     print (f"ERROR: Unknown symbol '{relocation['symbol']}'")
                     exit (1)
-                address = objectCode["symbols"][relocation["symbol"]]["relAddress"]
+                address = objectCode["symbols"][relocation["symbol"]]["absAddress"]
                 relocType = relocation["type"]
                 # Handle relocType
                 addressBytes = self.convertAddressToBytes (address, relocType)
@@ -80,6 +148,8 @@ class Linker:
                 offset = relocation['location']
                 objectCode["bytecode"][offset:offset+patchSize] = addressBytes
                 self.printDebug (f"Patched {relocation['symbol']} ({relocType}) at {offset:x} with {address:x}")
+
+    # ---------------------------------------------------------------------------------------------
 
     def flattenCodeSections (self, objectCodes):
         self.printDebug ("Flattening code sections...")
